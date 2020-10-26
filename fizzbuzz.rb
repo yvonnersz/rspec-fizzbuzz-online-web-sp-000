d2508c8769359579083c2ce227741e6360eeaaf6
Yvonne M Pham <yvonnersz@gmail.com>
2020-10-26 19:40:07 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,5 +2,10 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(number)
-
+  if number%3 === 0 && number%5 === 0
+    return "FizzBuzz"
+  elsif number%3 === 0
+    return "Fizz"
+  elsif number%5 === 0
+    return "Buzz"
 end
