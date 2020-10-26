f8b5f37209be71672415264720414947ec31ba1b
Yvonne M Pham <yvonnersz@gmail.com>
2020-10-26 19:36:27 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,8 +1,6 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(number)
-  if number%3 === 0
-    return "Fizz"
-  end
+def fizzbuzz
+
 end
