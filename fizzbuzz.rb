df4586c482c847d6067e9be925f513388342814e
Yvonne M Pham <yvonnersz@gmail.com>
2020-10-26 19:38:25 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,9 +1,8 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-class FizzBuzz
+def fizzbuzz
 
-  def fizzbuzz
-
-  end
 end
+
+fizzbuzz(3);
